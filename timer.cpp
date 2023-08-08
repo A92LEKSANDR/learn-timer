ee86289190a3d2ab216904ae9148e22e0cabd1a9
A92LEKSANDR <k92aco@gmail.com>
2023-08-08 16:22:54 +0300

    add choose path and input

--- timer.cpp
+++ timer.cpp
@@ -5,11 +5,13 @@
 #include <ctime>
 #include <Windows.h>
 #include <conio.h>
+#include <cstdlib>
 
 class Timer{
     int min;
+    std::string path;
     public:
-    Timer(int value):min(value){}
+    Timer(int value, std::string path_):min(value),path(path_){}
 
     void start(){
         std::time_t start = std::time(0);//?
@@ -35,23 +37,46 @@ class Timer{
     }
              
 }//while
-        std::fstream out;
-        out.open("C:/learn.txt",std::ios_base::app);
 
-        const char* currentTime = std::ctime(&start);
+        //save txt information time and turn off display
+        std::fstream out;
+        out.open(path,std::ios_base::app);
+
+        //block code for protection error input
+        if(!out.is_open()){
+            bool open = false;
+            while(!open){
+                std::cout << "path not found/ file.txt!\npress enter try again path: ";
+                std::cin >> path;
+                if(out.is_open()){
+                    open = true;
+                }
+            }
+        }//
+
+        const char* currentTime = std::ctime(&start);//convert time fot char[]
 
         out << "\ncurrent time: " << currentTime << "\n25 minuts of learn\nand five minuts time-out";
         out.close();
 
-        SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+        SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);//turn off display
         
     }
 };
 
 int main(){
 
-    Timer timer(25);
-    timer.start();
+    Timer timer(25, "C:/timer.txt"); // where be win application, press button chouse and input text
+    char option{'\0'};
+
+    do{
+        timer.start();
+        std::cout << "press enter y to continue/press enter n to exit: ";
+
+        std::cin >> option;
+
+    }while(option != 'n');
+    
 
     return 0;
 }
