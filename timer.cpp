e58ea908dd7cd4a9ad5f2154965899a048381d30
A92LEKSANDR <k92aco@gmail.com>
2023-08-08 12:47:55 +0300

    add time out

--- timer.cpp
+++ timer.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <ctime>
 #include <Windows.h>
+#include <conio.h>
 
 class Timer{
     int min;
@@ -18,15 +19,28 @@ class Timer{
              for(int i = 59; i > 0; --i){
              std::cout << "\nTime of minuts: " << min << " seconds: " << i << '\n';
              std::this_thread::sleep_for(std::chrono::seconds(1));//?
-              }
+            
+            //time out block
+            if(_kbhit()){
+            char ch = _getch();
+                if(ch == 'p' || ch == 'P'){
+                    //SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+                    for(int i = 300; i > 0; --i){
+                    std::cout << "\ntime-out seconds: " << i << '\n';
+                    std::this_thread::sleep_for(std::chrono::seconds(1));//?
+    
+                }
+            }
+        }
+    }
              
-        }//while
+}//while
         std::fstream out;
         out.open("C:/learn.txt",std::ios_base::app);
 
-        const char* currentTime = std::ctime(&start);//convert to char[]
+        const char* currentTime = std::ctime(&start);
 
-        out << "current time: " << currentTime << "\n25 minuts of leartn\nand five minuts time-out\n";
+        out << "\ncurrent time: " << currentTime << "\n25 minuts of learn\nand five minuts time-out";
         out.close();
 
         SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
